55bed544b8df378f91508cee4547e1c76d074302
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-04-12 19:41:31 +0700

    Fix modal

--- src/components/_base/Modal.vue
+++ src/components/_base/Modal.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="bg-modal">
+  <div class="bg-modal" @keyup.esc="$emit('close-modal')" >
     <div class="modal-content">
       <div class="modal-header">
         <button class="close" @click="$emit('close-modal')">
           <img src="@/assets/img/close.png" alt="">
         </button>
-        <h2>Add Book</h2>
+        <h2>{{ modal.title }}</h2>
       </div>
       <div class="modal-container">
-        <form @submit="sendData">
+        <form @submit.prevent="sendData">
           <div class="baris">
             <div class="kolom-3">
               <label for="title">Title</label>
             </div>
             <div class="kolom-9">
-              <input v-model="title" type="text" id="title">
+              <input v-model="modal.input.title" type="text" id="title">
             </div>
           </div>
           <div class="baris">
@@ -22,7 +22,7 @@
               <label for="description">Description</label>
             </div>
             <div class="kolom-9">
-              <textarea v-model="description"
+              <textarea v-model="modal.input.description"
               id="description" rows="4"></textarea>
             </div>
           </div>
@@ -31,7 +31,7 @@
               <label for="author">Author</label>
             </div>
             <div class="kolom-9">
-              <input v-model="author" type="text" id="author">
+              <input v-model="modal.input.author" type="text" id="author">
             </div>
           </div>
           <div class="baris">
@@ -39,7 +39,7 @@
               <label for="image">Image</label>
             </div>
             <div class="kolom-9">
-              <input id="file" type="file" ref="file"  v-on:change="handleUpload">
+              <input id="file" type="file" ref="file">
             </div>
           </div>
           <div class="baris">
@@ -47,11 +47,11 @@
               <label for="category">Category</label>
             </div>
             <div class="kolom-9">
-              <input v-model="category" type="text" id="category">
+              <input v-model="modal.input.category" type="text" id="category">
             </div>
           </div>
           <div class="baris">
-            <button class="btn-modal">Save</button>
+            <button class="btn-modal">{{ modal.button }}</button>
           </div>
         </form>
       </div>
@@ -60,43 +60,32 @@
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
-  name: 'ModalSidebar',
+  name: 'Modal',
+  props: ['modal'],
   data() {
     return {
       title: '',
       author: '',
       description: '',
+      file: '',
       category: '',
       error: '',
       code: 0,
     };
   },
   methods: {
-    handleUpload() {
-      // this.img = this.$refs.file.files;
-    },
-    sendData(e) {
-      const that = this;
-      e.preventDefault();
-      const formData = new FormData();
-      formData.append('title', this.title);
-      formData.append('description', this.description);
-      formData.append('author', this.author);
-      formData.append('img', this.$refs.file.files[0]);
-      formData.append('id_category', this.category);
-      axios.post(`http://${process.env.VUE_APP_ROOT_URL}/api/v1/book/insert`, formData)
-        .then((res) => {
-          if (res.status === 200) {
-            that.$router.push(`book/${res.data.result.insertId}`);
-          }
-        })
-        .catch((err) => {
-          that.error = err;
-          that.code = 1;
-        });
+    sendData() {
+      this.$emit('submit-data', this.$refs.file.files[0]);
+      // e.preventDefault();
+      // const formData = new FormData();
+      // formData.append('title', this.title);
+      // formData.append('description', this.description);
+      // formData.append('author', this.author);
+      // formData.append('img', this.$refs.file.files[0]);
+      // formData.append('id_category', this.category);
+      // this.$emit('data-modal', formData);
     },
   },
 };
